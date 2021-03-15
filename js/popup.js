05cca5d6457944160e8f43c4b111720889ea9059
Conor Watson <conor@odro.co.uk>
2021-03-15 10:33:28 +0000

    feat: create popup html, css and js

---
+++ js/popup.js
@@ -0,0 +1,72 @@
+function docReady(fn) {
+  // see if DOM is already available
+  if (document.readyState === "complete" || document.readyState === "interactive") {
+      // call on next available tick
+      setTimeout(fn, 1);
+  } else {
+      document.addEventListener("DOMContentLoaded", fn);
+  }
+} 
+
+docReady(function(){
+  var recording = false;
+  
+  // Start recording
+  function record(){
+      if (!recording) {
+          chrome.runtime.sendMessage({ type: "record" });
+      } else {
+          recording = false;
+          chrome.runtime.sendMessage({ type: "stop" });
+      }
+  }
+  
+  // Start recording
+  document.querySelector('#sc-button').addEventListener('click', record);
+  
+  // Receive messages
+  chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+      if (request.type == "loaded") {
+          window.close();
+      } 
+      // else if (request.type == "sources") {
+      //     getCamera(request.devices);
+          
+      //     // Allow user to start recording
+      //     if (!recording) {
+      //         $("#record").html(chrome.i18n.getMessage("start_recording"));
+      //     }
+      //     $("#record").removeClass("record-disabled");
+      // } else if (request.type == "sources-audio") {
+      //     getAudio(request.devices);
+          
+      //     // Allow user to start recording
+      //     if (!recording) {
+      //         $("#record").html(chrome.i18n.getMessage("start_recording"));
+      //     }
+      //     $("#record").removeClass("record-disabled");
+      // } else if (request.type == "sources-noaccess") {
+      //     $("#camera-select").html("<option value='disabled-access'>"+chrome.i18n.getMessage("disabled_allow_access")+"</option>");
+      //     $("#camera-select").niceSelect('update');
+      //     chrome.storage.sync.set({
+      //         camera: "disabled-access"
+      //     });
+          
+      //     // Allow user to start recording
+      //     if (!recording) {
+      //         $("#record").html(chrome.i18n.getMessage("start_recording"));
+      //     }
+      //     if ($(".type-active").attr("id") != "camera-only") {
+      //         $("#record").removeClass("record-disabled");
+      //     }
+      // } else if (request.type == "sources-loaded") {
+      //     chrome.tabs.getSelected(null, function(tab) {
+      //         chrome.tabs.sendMessage(tab.id, {
+      //             type: "camera-request"
+      //         });
+      //     });
+      // } else if (request.type == "sources-audio-noaccess") {
+      //     audioRequest();   
+      // }
+  });
+});
